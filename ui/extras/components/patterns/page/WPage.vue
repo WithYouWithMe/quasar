84c436610845d66eae3f1c8b0ef6be8a13e4f287
Uri Levitsky <uri@withyouwithme.com>
2019-06-05 09:53:56 +1000

    fixed page

--- ui/extras/components/patterns/page/WPage.vue
+++ ui/extras/components/patterns/page/WPage.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  q-page.w-page(:padding="padding")
-    .row(:class="{ 'offset-md-2': offset, 'col-md-8': offset }")
+  q-page.w-page.row(:padding="padding")
+    div(:class="{ 'offset-md-2': offset, 'col-md-8': offset }")
       .row.justify-between.items-end
         .text-page-title {{title}}
         slot(name="title-right")
