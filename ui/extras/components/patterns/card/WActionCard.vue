4d80261e6ff914458110d0b3a49313b66b54dd9f
Uri Levitsky <uri@withyouwithme.com>
2019-06-07 17:32:35 +1000

    wactopm card

--- ui/extras/components/patterns/card/WActionCard.vue
+++ ui/extras/components/patterns/card/WActionCard.vue
@@ -1,33 +1,40 @@
 <template lang="pug">
   w-card.w-card.w-action-card.cursor-pointer(flat bordered)
-    w-img(v-if="imgSrc" :src="imgSrc" :ratio="16/9" basic)
-      .absolute-bottom.q-ml-sm.q-mb-xs(v-if="chipLeft")
-        w-chip(:color="chipColor" :style="chipStyle") {{chipLeft}}
-    .items-lower.row.justify-between.items-end(v-if="!chipLeft && (chipRight || month || day)")
-      w-card.calendar.q-py-sm.q-px-md.q-ml-md(v-if="month || day")
-        .column.full-height.justify-center.items-center
-          .month {{month}}
-          .day {{day}}
-      .calendar-placeholder(v-else)
-      w-chip.q-mr-md(v-if="chipRight" :color="chipColor" :style="chipStyle") {{chipRight}}
-    w-card-section(v-if="title || subtitle")
-      .subtitle(v-if="subtitle") {{subtitle}}
-      .title(v-if="title") {{title}}
-    w-card-section.details(v-if="details")
-      .column
-        .row.q-gutter-sm(v-for="(detail, detailIndex) in details" :key="detailIndex")
-          w-icon(:name="detail.icon")
-          .text-caption {{detail.text}}
+    .column.full-height.justify-center.items-center(v-if="loading")
+      w-spinner(size="3em" color="grey-4")
+    template(v-else)
+      w-img(v-if="imgSrc" :src="imgSrc" :ratio="16/9" basic)
+        .absolute-bottom.q-ml-sm.q-mb-xs(v-if="chipLeft")
+          w-chip(:color="chipColor" :style="chipStyle") {{chipLeft}}
+      .items-lower.row.justify-between.items-end(v-if="!chipLeft && (chipRight || month || day)")
+        w-card.calendar.q-py-sm.q-px-md.q-ml-md(v-if="month || day")
+          .column.full-height.justify-center.items-center
+            .month {{month}}
+            .day {{day}}
+        .calendar-placeholder(v-else)
+        w-chip.q-mr-md(v-if="chipRight" :color="chipColor" :style="chipStyle") {{chipRight}}
+      w-card-section(v-if="title || subtitle")
+        .subtitle(v-if="subtitle") {{subtitle}}
+        .title(v-if="title") {{title}}
+      w-card-section.details(v-if="details")
+        .column
+          .row.q-gutter-sm(v-for="(detail, detailIndex) in details" :key="detailIndex")
+            w-icon(:name="detail.icon")
+            .text-caption {{detail.text}}
 </template>
 
 <script>
-import { WCard, WCardSection, WImg, WChip } from "quasar";
+import { WCard, WCardSection, WImg, WChip, WSpinner } from "quasar";
 
 export default {
   name: "WActionCard",
   mixins: [ WCard ],
   components: { WCard, WCardSection, WImg, WChip },
   props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
     imgSrc: {
       type: String,
       default: null,
